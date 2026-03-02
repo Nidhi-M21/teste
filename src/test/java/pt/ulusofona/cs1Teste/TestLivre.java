33b117724080f6ea01892bb0a71dab1e1ffa4545
Nidhi <a22405789@alunos.ulht.pt>
2026-03-02 12:42:19 +0000

    organizei a estrutura

---
+++ src/test/java/pt/ulusofona/cs1Teste/TestLivre.java
@@ -0,0 +1,12 @@
+package pt.ulusofona.cs1Teste;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestLivre {
+    @Test
+    public void testDummy() {
+        assertTrue(true);
+    }
+}
